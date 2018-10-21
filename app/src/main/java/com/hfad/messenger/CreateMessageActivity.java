f6e9f3a0da1acc9c5087c920507650b8a53acc9d
acb4905 <37702798+acb4905@users.noreply.github.com>
2018-10-21 16:20:03 -0400

    Initial commit

---
+++ app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -0,0 +1,27 @@
+package com.hfad.messenger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+    public void onSendMessage(View view){
+        EditText messageView= findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        Intent intent = new Intent (Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        String chooserTitle= getString(R.string.chooser);
+        //createChooser lets you choose a new app every time and not set a default app
+        Intent chosenIntent = Intent.createChooser(intent,chooserTitle);
+        startActivity(chosenIntent);
+    }
+}
